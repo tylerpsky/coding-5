42e4ebd5f40d3ff9d0d5a107f8caa7a91f71f945
Tyler Paiewonsky <tylerpsky@gmail.com>
2025-02-09 18:02:38 -0500

    Task 1 - Customer Profile.

---
+++ cc_5.js
@@ -0,0 +1,13 @@
+//Task 1
+
+//Declare the customer object
+const customer = {
+    name: "Markum Reed",
+    age: 35,
+    email: "markumreed@usf.edu"
+};
+
+//Log each property
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
